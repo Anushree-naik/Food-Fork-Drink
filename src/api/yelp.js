4b2472101b6b25f8cc4602f08821db252fab08e1
Anushree Naik <anushree317@gmail.com>
2019-10-26 22:53:51 -0400

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,7 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization:
-            'Bearer FlG-H_TaiuiEdp7zhiLgFZ-Uy4KQSxOYce-REKreXCTxcB2h6YpCUG1zRO-nsA5GJ4Cescr2Bgz4BWRDw09LpJG5ELczqANneNCIDCkJbQ1B2Cm3TkniZFlip7BNXXYx'
+            'Bearer Your API KEY goes here(Syntax: Bearer'space'APIkey)'
     }
 });
 
